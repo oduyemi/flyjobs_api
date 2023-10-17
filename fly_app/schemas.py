75c3b972269b6cfb65574c233f4703c070169e97
Yemi Oduyemi <yhermii@gmail.com>
2023-10-17 08:56:24 +0100

    Merge to Master

--- fly_app/schemas.py
+++ fly_app/schemas.py
@@ -2,3 +2,11 @@ from datetime import datetime
 from pydantic import BaseModel
 from typing import List, Optional, Dict, Union
 
+class JobRequest(BaseModel):
+    name: str
+    description: str
+
+class JobResponse(BaseModel):
+    id: int
+    name: str
+    description: str
